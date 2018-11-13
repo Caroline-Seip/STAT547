bd2ddcb48684b20d88a46f32c787763ba33eadca
Caroline Seip <seip@unbc.ca>
2018-11-13 10:07:44 -0800

    To break

--- bcl/app.R
+++ bcl/app.R
@@ -1,9 +1,23 @@
 library(shiny)
 
+a <- 5
+print(a^2)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage( "This is some text",
                  "This is more text",
-                 tags$h1("Level 1 header")
+                 tags$h1("Level 1 header"),
+                 h1("level 1 header, part 2"),
+                 HTML("<h1>Level 1 header, part 3<h1>"),
+                 tags$mark("Caribou"),
+                 tags$address("4778"),
+                 tags$button("Hello"),
+                 br(),
+                 br(),
+                 br(),
+                 tags$strong("Strong"),
+                 br(),
+                 print(a)
 )
 
 # Define server logic required to draw a histogram
