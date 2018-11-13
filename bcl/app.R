2445f15a2765130fc51420f106698445430cba0a
Caroline Seip <seip@unbc.ca>
2018-11-13 10:42:28 -0800

    Final commit

--- bcl/app.R
+++ bcl/app.R
@@ -4,25 +4,21 @@ a <- 5
 print(a^2)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage( "This is some text",
-                 "This is more text",
-                 tags$h1("Level 1 header"),
-                 h1("level 1 header, part 2"),
-                 HTML("<h1>Level 1 header, part 3<h1>"),
-                 tags$mark("Caribou"),
-                 tags$address("4778"),
-                 tags$button("Hello"),
-                 br(),
-                 br(),
-                 br(),
-                 tags$strong("Strong"),
-                 br(),
-                 print(a)
-)
 
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(     titlePanel("BC Liquor price app", 
+                                windowTitle = "BCL app"),
+                     sidebarLayout(
+                       sidebarPanel("This text is in the sidebar."),
+                       mainPanel(
+                         plotOutput("price_hist")),
+                       tableOutput("price_hist")
+                     )
+)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+   output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
 }
 
 # Run the application 
